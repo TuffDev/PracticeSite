05a06ac1cd16addc961e33bd3b0acfa2ad3a1033
TuffDev <cooladam98@gmail.com>
2015-02-15 00:55:59 -0500

    Initial Commit

---
+++ index.html~
@@ -0,0 +1,5 @@
+<?php
+
+print ("Adam is awesome");
+
+?>
---
+++ jquery/custom.js
@@ -0,0 +1,6 @@
+/* Filename: custom.js */
+$(document).ready(function() {
+  $("div").click(function() {
+	$("#divid").text("YOU Idiot why would you click that?");
+  });
+});
